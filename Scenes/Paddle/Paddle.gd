021872a414155bfb819cf6adba2136e25c2bf64c
RamzesKh <ramzeshotambekzofa@gmail.com>
2025-09-27 22:48:42 +0200

    Add Paddle movement script
    Introduces Paddle.gd for Area2D paddle control. Handles horizontal movement using input axis and clamps position within viewport bounds.

---
+++ Scenes/Paddle/Paddle.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+const SPEED: float = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var movement = Input.get_axis("Left", "Right")
+	position.x += delta * movement * SPEED
+	position.x = clampf(
+		position.x,
+		get_viewport_rect().position.x,
+		get_viewport_rect().end.x
+		
+	)
