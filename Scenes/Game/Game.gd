4e3f320a5a7b66260c278bad3858c172863942e8
RamzesKh <ramzeshotambekzofa@gmail.com>
2025-10-01 18:31:34 +0200

    Update Game.gd

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
-
+@onready var paddle: Area2D = $Paddle
+@onready var spawn_timer: Timer = $Timer
 
 const GEM = preload("res://Scenes/Gem/Gem.tscn")
+const MARGIN = 20
+
 
 func _ready() -> void:
 	spawn_gem()
 	
-
+func stop_all() -> void:
+	spawn_timer.stop()
+	
+	for child in get_children():
+		if child is Gem:
+			child.set_process(false)
+	paddle.set_process(false)
+	
+	
 func spawn_gem() -> void:
-	var new_gen = GEM.instantiate()
-	var x_pos: float = randf_range(0.0, get_viewport_rect().size.x-10)
+	var new_gen : Gem = GEM.instantiate()
+	var x_pos: float = randf_range(get_viewport_rect().position.x + MARGIN, get_viewport_rect().size.x-MARGIN)
+
 	new_gen.position = Vector2(x_pos, -50.0)
+	new_gen.gem_off_screen.connect(_on_gem_off_screen)
 	add_child(new_gen)
 	
 	
@@ -21,7 +34,7 @@ func _on_paddle_area_entered(area: Area2D) -> void:
 	
 	
 func _on_gem_off_screen() -> void:
-	pass # Replace with function body.
+	stop_all()
 
 
 func _on_timer_timeout() -> void:
